725ff69b1d5f1013e14c68a5ae78a6f134175982
Jude Lee <uuhh5417@gmail.com>
2020-07-01 14:18:55 +0900

    Create SignActivity

--- app/src/main/java/com/example/covid_tracing_app/LoginActivity.java
+++ app/src/main/java/com/example/covid_tracing_app/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.covid_tracing_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnSignup = (Button)findViewById(R.id.buttonSignup);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
