8d0852d8b7fad163cc8f7539eb11107f1c0789fb
Jude Lee <uuhh5417@gmail.com>
2020-07-10 16:31:39 +0900

    Develop bluetooth

--- app/src/main/java/com/example/covid_tracing_app/MainActivity.java
+++ app/src/main/java/com/example/covid_tracing_app/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.covid_tracing_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int REQUEST_ENABLE_BT = 420;
+    TextView textStatus;
+    BluetoothAdapter bluetoothAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        textStatus = (TextView)findViewById(R.id.textViewStatus);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(bluetoothAdapter == null){
+            //블루투스가 지원하지 않을 경우
+        }
+        else{
+            if(!bluetoothAdapter.isEnabled()){
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
     }
 }
